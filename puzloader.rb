7fce89f2d6ceeb3cec56c7dbb231dc598f8fe8bb
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-08 16:49:07 +0100

    Splt out debugging to a new class.

--- puzloader.rb
+++ puzloader.rb
@@ -15,14 +15,14 @@ class PuzzleLoader
   attr_reader :width, :height, :rows, :num_clues, :clues, :title, :author,
               :copyright
 
-  def initialize(filename, debug = false)
+  def initialize(filename)
     @buffer = PuzzleBuffer.new(read filename)
-    # @debug = debug
 
     # Skip past an optional pre-header
     seek_to(SIGNATURE, -2)
 
-    debug ? debug_check_values : seek_by(2 + 12 + 2 + 4 + 4 + 4 + 2 + 2 + 12)
+    # Skip unimportant header fields
+    seek_by(2 + 12 + 2 + 4 + 4 + 4 + 2 + 2 + 12)
 
     load_common
   end
@@ -41,11 +41,6 @@ class PuzzleLoader
 
   private
 
-  def debug_check_values
-    load_check_values
-    show_check_values
-  end
-
   def load_common
     load_size
     load_answer
@@ -54,45 +49,16 @@ class PuzzleLoader
     load_clues
   end
 
-  def load_check_values
-    seek_to(SIGNATURE, -2)
-
-    @file_checksum  = unpack('S<')
-    @sig            = unpack_zstring
-    @cib_checksum   = unpack('S<')
-    @lowparts       = unpack_multiple('C4', 4)
-    @highparts      = unpack_multiple('C4', 4)
-    @version        = unpack('Z4', 4)
-    @reserved1c     = unpack('S<')
-    @scrambled_sum  = unpack('S<')
-    @reserved20     = unpack_multiple('C12', 12)
-  end
-
-  def show_check_values
-    debug 'File Checksum', @file_checksum
-    puts "Signature: #{@sig}"
-    debug 'CIB Checksum', @cib_checksum
-    pp @lowparts, @highparts
-    puts "Version: #{@version}"
-    debug 'Reverved?', @reserved1c
-    debug 'Scrambled Checksum', @scrambled_checksum || 0
-    pp @reserved20
-  end
-
   def load_size
     @width, @height, @num_clues = unpack_multiple('C2S<', 4)
     # Puzzle Type, 1 = Normal, 0x0401 = Diagramless
     seek_by(2)
     @scrambled = unpack('S<')
-
-    # puts "Sizes: #{@width}, #{@height}, #{@num_clues}" if @debug  # TODO
   end
 
   def load_answer
     @rows = []
     @height.times { @rows << unpack('a' + @width.to_s, @width) }
-
-    # pp @rows if @debug  # TODO
   end
 
   # Skip possible solution
@@ -104,15 +70,11 @@ class PuzzleLoader
     @title      = unpack_zstring
     @author     = unpack_zstring
     @copyright  = unpack_zstring
-
-    # puts "Texts: #{@title}, #{@author}, #{@copyright}" if @debug   # TODO
   end
 
   def load_clues
     @clues = []
     @num_clues.times { @clues << unpack_zstring }
-
-    # pp @clues if @debug   # TODO
   end
 
   def read(filename)
@@ -127,5 +89,49 @@ class PuzzleLoader
 
   def debug(name, value)
     printf "%s: %d 0x%04x\n", name, value, value
+  end
+end
+
+# Debg version of puzzle loader that shows more of the header
+# :reek:TooManyInstanceVariables - More than half are only used for debugging.
+class DebugPuzzleLoader
+  def initialize(filename)
+    @buffer = PuzzleBuffer.new(read filename)
+
+    # Skip past an optional pre-header
+    seek_to(SIGNATURE, -2)
+
+    load_check_values
+    show_check_values
+
+    load_common
+  end
+
+  # :reek:TooManyStatements
+  # :reek:UncommunicativeVariableName - I think they're fine
+  def load_check_values
+    seek_to(SIGNATURE, -2)
+
+    @file_checksum  = unpack('S<')
+    @sig            = unpack_zstring
+    @cib_checksum   = unpack('S<')
+    @lowparts       = unpack_multiple('C4', 4)
+    @highparts      = unpack_multiple('C4', 4)
+    @version        = unpack('Z4', 4)
+    @reserved1c     = unpack('S<')
+    @scrambled_sum  = unpack('S<')
+    @reserved20     = unpack_multiple('C12', 12)
+  end
+
+  # :reek:TooManyStatements
+  def show_check_values
+    debug 'File Checksum', @file_checksum
+    puts "Signature: #{@sig}"
+    debug 'CIB Checksum', @cib_checksum
+    pp @lowparts, @highparts
+    puts "Version: #{@version}"
+    debug 'Reverved?', @reserved1c
+    debug 'Scrambled Checksum', @scrambled_checksum || 0
+    pp @reserved20
   end
 end

407c75dfcbe71a474075e822797da85989f1b01e
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-08 16:49:53 +0100

    Add debugging function to show loaded values.

--- load_puzzle.rb
+++ load_puzzle.rb
@@ -1,6 +1,17 @@
 #! /usr/bin/env ruby -I.
 
 require 'puzloader'
+require 'grid'
+
+def debug_loader(loader)
+  puts "Texts:\n  #{loader.title}\n  #{loader.author}\n  #{loader.copyright}"
+  puts "\nDimensions: #{loader.width} x #{loader.height}"
+  print "\nRows:\n  ", loader.rows.join("\n  "), "\n"
+  print "\nClues:\n  ", loader.clues.join("\n  "), "\n"
+end
 
 filename = ARGV[0] || 'Puzzles/2014-4-22-LosAngelesTimes.puz'
-loader = PuzzleLoader.new(filename, :debug)
+loader = PuzzleLoader.new(filename)
+debug_loader(loader)
+
+grid = Grid.new(loader.rows, loader.clues)
